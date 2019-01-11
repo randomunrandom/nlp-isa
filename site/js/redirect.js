f701bea14e4675ab99ad75712a34ef1a617ba852
randomunrandom <randomunrandom@yandex.ru>
2019-01-11 16:11:21 +0300

    added redirect

--- site/js/redirect.js
+++ site/js/redirect.js
@@ -2,6 +2,18 @@
   "use strict"; // Start of use strict
 
   let userLang = navigator.language || navigator.userLanguage;
-  console.log(userLang);
-
+  console.log(userLang.split('-')[0]);
+  switch (userLang.split('-')[0]) {
+    case 'en':
+      window.location.replace(window.location.href.replace("index", "en/index"));
+      break;
+    case 'ru':
+      window.location.replace(window.location.href.replace("index", "ru/index"));
+      break;
+    default:
+      window.location.replace(window.location.href.replace("index", "ru/index"));
+      break;
+  }
+  
+  
 })(jQuery); // End of use strict
