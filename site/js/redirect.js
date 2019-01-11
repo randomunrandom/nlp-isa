1095536bba4bbc46f6a9b83959170c4c39518bd5
randomunrandom <randomunrandom@yandex.ru>
2019-01-11 16:13:04 +0300

    added redirect

--- site/js/redirect.js
+++ site/js/redirect.js
@@ -4,9 +4,9 @@
   let userLang = navigator.language || navigator.userLanguage;
   console.log(userLang.split('-')[0]);
   switch (userLang.split('-')[0]) {
-    case 'en':
-      window.location.replace(window.location.href.replace("index", "en/index"));
-      break;
+    // case 'en':
+    //   window.location.replace(window.location.href.replace("index", "en/index"));
+    //   break;
     case 'ru':
       window.location.replace(window.location.href.replace("index", "ru/index"));
       break;
