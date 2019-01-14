372632db446c06e684376ebbfc3506348808f97c
randomunrandom <randomunrandom@yandex.ru>
2019-01-14 23:43:37 +0300

    cleanup

--- site/js/redirect.js
+++ site/js/redirect.js
@@ -3,15 +3,31 @@
 
   console.log(window.location.href);
   let userLang = navigator.language || navigator.userLanguage;
+  let path = window.location.href;
   switch (userLang.split('-')[0]) {
     // case 'en':
-    //   window.location.replace(window.location.href + "en/index.html");
-    //   break;
+      // if (path.includes('index')) {
+      //   window.location.replace( path.replace('index', 'en/index'));
+      // }
+      // else {
+      //   window.location.replace( path + "en/index.html");
+      // }
+      // break;
     case 'ru':
-      window.location.replace(window.location.href + "ru/index.html");
+      if (path.includes('index')) {
+        window.location.replace( path.replace('index', 'ru/index'));
+      }
+      else {
+        window.location.replace( path + "ru/index.html");
+      }
       break;
     default:
-      window.location.replace(window.location.href + "ru/index.html");
+      if (path.includes('index')) {
+        window.location.replace( path.replace('index', 'ru/index'));
+      }
+      else {
+        window.location.replace( path + "ru/index.html");
+      }
       break;
   }
   
