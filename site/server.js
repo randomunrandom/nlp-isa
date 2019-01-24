ee9dc296e8f70c8d73020495eb6a29027716811e
randomunrandom <randomunrandom@yandex.ru>
2019-01-24 21:36:13 +0300

    server now serves pages without .html

--- site/server.js
+++ site/server.js
@@ -9,6 +9,12 @@ const server = http.createServer((request, response) => {
   const req_id = uid.token(true).substr(0, 8);
   console.log(`[${new Date().toJSON().replace("T", " ").replace("Z", "")} - ${req_id}] ${request.method} ${request.url}`);
   const serve = serveStatic("./site/");
+  console.log("req = ", request.url);
+  if (request.url.indexOf('.') < 0) {
+    if (request.url.indexOf('?') < 0) {request.url = request.url + ".html";}
+    else {request.url = request.url.slice(0, request.url.indexOf('?'))+ ".html" + request.url.slice(request.url.indexOf('?'));}
+  }
+  // console.log("response = ", response);
   const done = finalhandler(request, response);
   serve(request, response, () => {
     console.log(`[${new Date().toJSON().replace("T", " ").replace("Z", "")} - ${req_id}] ${response.statusCode} ${http.STATUS_CODES[response.statusCode]}`);
