52b3ad4e82a067701f3541095255ddac8438f5e6
randomunrandom <randomunrandom@yandex.ru>
2019-01-25 18:28:00 +0300

    changed name to actual name

--- site/js/projects.js
+++ site/js/projects.js
@@ -1,13 +1,15 @@
 (function($) {
-  "use strict"; // Start of use strict
+  "use strict";
 
   axios.get('../assets/data/projects.json')
     .then(function (response) {
       let lang = window.location.href.indexOf('en') >= 0 ? 'en' : 'ru';
+      let data;
       Object.keys(response['data']['data_' + lang]).forEach(function(key) {
+        data = response['data']['data_'+lang][key];
         $('#list').append('<li><div class="container">' +
-          '<h2 style="display: inline;">Навзание: </h2><h4 style="display: inline;">' + key + '</h4><br>' +
-          '<h3 style="display: inline;">' + response['data']['data_'+lang][key]['short_description'] + '</h3><br>' +
+          '<h2 style="display: inline;">Навзание: </h2><h4 style="display: inline;">' + data['name'] + '</h4><br>' +
+          '<h3 style="display: inline;">' + data['short_description'] + '</h3><br>' +
           '<a class="btn btn-warning" href="' +
           window.location.href.replace("projects", "project") + '?project_id=' + key +
           '">Страница проекта</a></div></li>');
