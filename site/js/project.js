a5a4cc5b2470c34fa0ada3fb3658ccc3899664c8
randomunrandom <randomunrandom@yandex.ru>
2019-01-25 20:07:36 +0300

    some cleanup

--- site/js/project.js
+++ site/js/project.js
@@ -1,25 +1,17 @@
 (function($) {
   "use strict";
   let project_id;
-  // if (window.location.href.indexOf('?') >= 0) {
-    const url_params = new URLSearchParams(window.location.search);
-    project_id = url_params.get('project_id');
-  // }
-  // else {
-  //   const href_list = window.location.href.split('/');
-  //   project_id = href_list[href_list.length - 1];
-  // }
-  // console.log(project_id);
+  const url_params = new URLSearchParams(window.location.search);
+  project_id = url_params.get('project_id');
+
   axios.get('../assets/data/projects.json')
     .then(function (response) {
-      // console.log(response);
       let lang = window.location.href.indexOf('en') >= 0 ? 'en' : 'ru';
       let data;
       let str_params = ['name', 'description'];
       let list_params = ['demo', 'datasets', 'publications'];
       str_params.forEach(function(key) {
         data = response['data']['data_' + lang][project_id][key];
-        // console.log(data);
         if (data.length > 0) {
           $('#' + key).append('<div class="container"><p>' + data + '</p></div>');
         }
@@ -29,7 +21,6 @@
       });
       list_params.forEach(function(key) {
         data = response['data']['data_' + lang][project_id][key];
-        // console.log(data);
         if (data.length === 1) {
           $('#' + key).append('<div class="container"><p>' + data + '</p></div>');
         }
@@ -48,7 +39,6 @@
 
     })
     .catch(function (error) {
-      // console.log(error);
       // window.location.href = window.location.href.replace('project', '404');
     });
 
