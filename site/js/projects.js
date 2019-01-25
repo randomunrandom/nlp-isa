68267bdecc60ecce73fe389e643dd9ffb8e4514d
randomunrandom <randomunrandom@yandex.ru>
2019-01-25 14:34:27 +0300

    sme changes according to TR and added 404 error page

--- site/js/projects.js
+++ site/js/projects.js
@@ -1,12 +1,31 @@
+// import {AxiosInstance as axios} from "axios";
+
 (function($) {
   "use strict"; // Start of use strict
 
   let link;
 
+  // axios.interceptors.response.use(function (response) {
+  //   if (response.status.toString() !== 'OK') {
+  //     return Promise.reject(response);
+  //   }
+  //   return response;
+  // }, function (error) {
+  //   Do something with response error
+    // return Promise.reject(error);
+  // });
+  axios.get('../assets/data/projects.json')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+      window.location.href = window.location.href.replace('projects', '404');
+    });
   // '<div class="container" id="name">\n\t<h1>Название:</h1>\n</div>\n" +
   // '<div class="container" id="description">\n\t<h2>Описание:</h2>\n</div>\n' +
   // '<div class="container" id="link">\n\t<h2>страница проекта:</h2>\n</div>'
-  $.getJSON( "../assets/data/projects.json", function(data_dict) {
+  $.getJSON('../assets/data/projects.json', function(data_dict) {
     Object.keys(data_dict["data"]).forEach(function(key) {
       link = '<a class="btn btn-sm" href="' +
         window.location.href.replace("projects", "project") + '?project_id=' + key +
