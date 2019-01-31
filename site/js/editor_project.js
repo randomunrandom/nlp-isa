118a7ad2093601a1c450cf7be1848a8d586b78bf
randomunrandom <randomunrandom@yandex.ru>
2019-01-31 19:51:15 +0300

    expanded codemirrors defenition

--- site/js/editor_project.js
+++ site/js/editor_project.js
@@ -1,9 +1,24 @@
 (function($) {
-  let editor_ids = ['short_description', 'description', 'demos', 'datasets', 'publications'];
-  editor_ids.forEach(
-    CodeMirror.fromTextArea(document.getElementById("editor"), {
-      lineNumbers: true
-      // mode:  "xml"
-      })
-  );
+  // let editor_ids = ['short_description', 'description', 'demos', 'datasets', 'publications'];_
+  let short_desc_editor = CodeMirror.fromTextArea(document.getElementById("short_desc_editor", {
+    lineNumbers: true
+    // mode:  "xml"
+  }));
+  let description_editor = CodeMirror.fromTextArea(document.getElementById("description_editor", {
+    lineNumbers: true
+    // mode:  "xml"
+  }));
+  let demos_editor = CodeMirror.fromTextArea(document.getElementById("demos_editor", {
+    lineNumbers: true
+    // mode:  "xml"
+  }));
+  let datasets_editor = CodeMirror.fromTextArea(document.getElementById("datasets_editor", {
+    lineNumbers: true
+    // mode:  "xml"
+  }));
+  let publications_editor = CodeMirror.fromTextArea(document.getElementById("publications_editor", {
+    lineNumbers: true
+    // mode:  "xml"
+  }));
+
 })(jQuery);
