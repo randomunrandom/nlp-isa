70547b350d44cbf603c12112e1263a44378f4f8c
randomunrandom <randomunrandom@yandex.ru>
2019-02-03 15:26:33 +0300

    added

--- site/Gulpfile.js
+++ site/Gulpfile.js
@@ -1,16 +1,18 @@
 const {src, dest, parallel} = require('gulp');
+let cleanCSS = require('gulp-clean-css');
 const babel = require('gulp-babel');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 
 function css() {
-//   return src('client/templates/*.less')
-//     .pipe(minifyCSS())
-//     .pipe(dest('build/css'))
+  return src(['css/*.css', '!css/clean-*.css'])
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({ prefix: 'clean-' }))
+    .pipe(dest('css'))
 }
 
 function js() {
-  return src('js/*.js')
+  return src(['js/*.js', '!js/*.min.js'])
     .pipe(babel({
       presets: ['@babel/env']
     }))
