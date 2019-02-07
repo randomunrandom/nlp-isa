563507c7fdbead768d00724fa0db37f7aeefe8e1
randomunrandom <randomunrandom@yandex.ru>
2019-02-07 14:56:59 +0300

    added demo

--- site/js/demo.js
+++ site/js/demo.js
@@ -8,40 +8,32 @@
 
   axios.get('../../assets/' + lang + '/data/demos.json')
     .then((response) => {
-      console.log(response);
+      // console.log(response);
       if (!(project_id in response['data'])) {
         window.location.href = '/' + lang + '/404';
       }
-      let data = response['data'][project_id];
-      $('#name').html('<h4>' + data['name'] + '</h4>');
-      $('#date').html('<p class="text-justify">Дата начала: ' + data['year'] + '</p>');
-      let replace_params = ['short_description'];
-      let str_params = ['description'];
-      replace_params.forEach((key) => {
-        data = response['data'][project_id][key];
-        console.log(data);
-        $('#' + key).html('<div class="container"><p class="text-justify">' + data + '</p></div>');
-      });
-      str_params.forEach((key) => {
-        data = response['data'][project_id][key];
-        if (data.length > 0) {
-          $('#' + key).append('<div class="container"><p class="text-justify">' + data + '</p></div>');
+      if (response['data'][project_id]['link'] === null) {
+        window.location.href = window.location.href.replace('/demos', '/demo/') + project_id;
+      }
+      let params = ['name', 'short_description', 'description', 'year', 'link'];
+      params.forEach((key) => {
+        let data;
+        if (key === 'link') {
+          response['data'][project_id]['link'] = '<a class="btn btn-md bg-warning" href="' + response['data'][project_id]['link']
+            + '">Страница демо инструмента</a>';
         }
-        else {
+        data = response['data'][project_id][key] || '';
+        // console.log(data);
+         if (data.length > 0) {
+           $('#' + key + ' div:last-child').html(data);
+        } else {
           $('#' + key).hide();
         }
       });
-      data = response['data'][project_id]['link'];
-      if(data.length > 0) {
-        $('#link').append('<div class="container"><a class="btn-link" href="' + data + '">' + data +'</a></div>');
-      }
-      else {
-        $('#' + link).hide();
-      }
     })
     .catch((error) => {
       console.log(error);
-      // window.location.href = window.location.href.replace('project', '404');
+      // window.location.href = window.location.href.replace('demos', '404');
     });
 
 })(jQuery);
